ff6fc7adcb844fb4ea1d726c6a52de420d8d2b45
Anthony Cunningham <oxa547@gmail.com>
2019-04-30 21:16:06 -0500

    commit 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { auth } from 'firebase/app';
+
+import { FirebaseUISignInSuccessWithAuthResult, FirebaseUISignInFailure } from 'firebaseui-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   title = 'Hint2';
+  uiConfig = {
+    signInSuccessUrl: 'http://localhost:4200/',
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      auth.GoogleAuthProvider.PROVIDER_ID,
+      auth.EmailAuthProvider.PROVIDER_ID
+    ]
+  };
 
   constructor(public afAuth: AngularFireAuth) {
-  }
-
-  login() {
-    this.afAuth.auth.signInWithPopup(new auth.GoogleAuthProvider());
-  }
-
-  logintwo() {
-    this.afAuth.auth.signInWithEmailAndPassword('oxa547@gmail.com', 'anewkindofpw');
+    this.afAuth.authState.subscribe(this.firebaseAuthChangeListener);
   }
 
   logout() {
     this.afAuth.auth.signOut();
+  }
+
+  private firebaseAuthChangeListener(response) {
+    // if needed, do a redirect in here
+    if (response) {
+      console.log('Logged in :)');
+      console.log(response);
+    } else {
+      console.log('Logged out :(');
+    }
+  }
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult) {
+    // console.log('success callback');
+  }
+
+  errorCallback(errorData: FirebaseUISignInFailure) {
+    // console.log('fail callback');
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,33 @@ import { AngularFireModule } from '@angular/fire';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
+import { FirebaseUIModule } from 'firebaseui-angular';
+import * as firebase from 'firebase';
+import * as firebaseui from 'firebaseui';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      scopes: [
+        'public_profile',
+        'email',
+        'user_likes',
+        'user_friends'
+      ],
+      customParameters: {
+        auth_type: 'reauthenticate'
+      },
+      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    },
+    firebase.auth.EmailAuthProvider.PROVIDER_ID
+  ],
+  // tosUrl: '<your-tos-link>',
+  // privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
+  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -18,7 +45,8 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
