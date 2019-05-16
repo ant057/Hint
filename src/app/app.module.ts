a42a4dc63495b321f11cd8b379ed62eef16bf6e2
Anthony Cunningham <oxa547@gmail.com>
2019-05-15 21:25:35 -0500

    commit itt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+import { CoreModule } from './core/core.module';
+import { AuthModule } from './auth/auth.module';
+
 import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,37 +14,16 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { FirebaseUIModule } from 'firebaseui-angular';
-import * as firebase from 'firebase';
-import * as firebaseui from 'firebaseui';
+import { firebaseUiAuthConfig } from './firebase/firebaseui.config';
 
-const firebaseUiAuthConfig: firebaseui.auth.Config = {
-  signInFlow: 'popup',
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    {
-      scopes: [
-        'public_profile',
-        'email',
-        'user_likes',
-        'user_friends'
-      ],
-      customParameters: {
-        auth_type: 'reauthenticate'
-      },
-      provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID
-    },
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ],
-  // tosUrl: '<your-tos-link>',
-  // privacyPolicyUrl: '<your-privacyPolicyUrl-link>',
-  credentialHelper: firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
-};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
+    AuthModule,
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
---
+++ src/app/core/data.service.ts
@@ -0,0 +1,27 @@
+// angular
+import { Injectable } from '@angular/core';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+  AngularFirestoreCollection,
+  DocumentChangeAction,
+  Action,
+  DocumentSnapshotDoesNotExist,
+  DocumentSnapshotExists,
+} from 'angularfire2/firestore';
+import * as firebase from 'firebase/app';
+
+import { environment } from '../../environments/environment';
+
+// rxjs
+import { Observable, from } from 'rxjs';
+import { map, tap, take, switchMap, mergeMap, expand, takeWhile } from 'rxjs/operators';
+
+// Models
+
+@Injectable()
+export class FirebaseService {
+
+  constructor(private afs: AngularFirestore) { }
+
+}
